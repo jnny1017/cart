11c239cefb2be54779f0ceaf237d69e57b56fad1
jnny1017 <jnny1017@gmail.com>
2022-09-04 16:40:35 +0900

    feat: Spinner 컴포넌트 생성

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import {
+  RoundSpinner,
+} from '../styles/menuStyle';
+
+export default function Spinner() {
+  return (
+    <RoundSpinner>
+      <span>Loading</span>
+    </RoundSpinner>
+  );
+}
